f9a1aad9574129971878aa797f4f06b66eac4b31
Chris <chris8119123@yahoo.com>
2016-11-17 02:27:02 -0600

    functional fg_login script

--- fg_login.py
+++ fg_login.py
@@ -1,31 +1,41 @@
 #! python3
 
-import webbrowser, sys, pyperclip, requests, selenium, bs4
+import webbrowser, sys, pyperclip, requests, selenium, bs4, time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+#Globals
 initial_browser_url = "https://www.fieldglass.net/time_sheet_form.do"
 
 def instantiate_Ie():
+  #Set initial browser url
   webdriver.DesiredCapabilities.INTERNETEXPLORER["initialBrowserUrl"] = initial_browser_url
+  #start the Ie browser
   browser = webdriver.Ie()
   return browser
 
+#Globalize the instantiated Ie webdriver
 driver = instantiate_Ie()
-fieldglass_username = "field glass username goes here"
-fieldglass_password = "fieldglass password goes here"
-driver.implicitly_wait(3)
-elem = driver.find_element_by_name('username')
-elem.clear()
-elem.send_keys(fieldglass_username)
-elem = driver.find_element_by_name('password')
-elem.send_keys(fieldglass_password)
-elem.submit()
+time.sleep(5)
 
+def login_fg(): 
+  #fg form values
+  fieldglass_username = "Itrius"
+  fieldglass_password = "Chris101"
+
+  #username input
+  elem = driver.find_element_by_name('username')
+  elem.clear()
+  elem.send_keys(fieldglass_username)
+  #password input
+  elem = driver.find_element_by_name('password')
+  elem.send_keys(fieldglass_password)
+  elem.submit()
+
+login_fg()
 
 
 
